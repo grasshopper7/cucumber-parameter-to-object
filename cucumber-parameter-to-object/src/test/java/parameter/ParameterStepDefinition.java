855670c8ca9adb96a32f61529d06895b01b9b7ca
grasshopper7 <grass.hopper.moc@gmail.com>
2019-08-17 15:09:26 +0530

    Initial Commit

--- cucumber-parameter-to-object/src/test/java/parameter/ParameterStepDefinition.java
+++ cucumber-parameter-to-object/src/test/java/parameter/ParameterStepDefinition.java
@@ -12,16 +12,6 @@ public class ParameterStepDefinition {
 		System.out.println(orderLines);
 	}
 
-	@Given("Dishes in menu")
-	public void dishes_in_Menu(List<Dish> dishes) {
-		System.out.println(dishes);
-	}
-
-	@Given("Menu dishes")
-	public void menu_Dishes(Menu menu) {
-		System.out.println(menu);
-	}
-
 	@Given("Customer orders the below dishes")
 	public void customer_Orders_the_below_dishes(Map<OrderLineId, OrderLine> orderLines) {
 		System.out.println(orderLines);
@@ -30,5 +20,15 @@ public class ParameterStepDefinition {
 	@Given("Customer orders the following dishes")
 	public void customer_Orders_the_followng_dishes(Map<Integer, OrderLine> orderLines) {
 		System.out.println(orderLines);
+	}
+
+	@Given("Dishes in menu")
+	public void dishes_in_Menu(List<Dish> dishes) {
+		System.out.println(dishes);
+	}
+
+	@Given("Menu dishes")
+	public void menu_Dishes(Menu menu) {
+		System.out.println(menu);
 	}
 }
