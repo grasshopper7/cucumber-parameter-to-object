66a993f433a1987361b1cb7c113fa6d8b96a3981
grasshopper7 <grass.hopper.moc@gmail.com>
2019-08-18 01:14:08 +0530

    Initial Commit

--- cucumber-parameter-to-object/src/test/java/parameter/ParameterStepDefinition.java
+++ cucumber-parameter-to-object/src/test/java/parameter/ParameterStepDefinition.java
@@ -4,25 +4,24 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
-import cucumber.api.java.en.Given;
+import cucumber.api.java8.En;
 
-public class ParameterStepDefinition {
+public class ParameterStepDefinition implements En {
 
-	@Given("Customer orders at {orderTime} on {orderDate}")
-	public void customer_orders_at_on(LocalTime time, LocalDate date) {
-		String timeText = time.format(DateTimeFormatter.ofPattern("h:m:s a"));
-		String dateText = date.format(DateTimeFormatter.ofPattern("d MMM yyyy"));
-		System.out.println("Order paced at " + timeText + " on " + dateText);
-	}
+	public ParameterStepDefinition() {
+		Given("Customer orders at {orderTime} on {orderDate}", (LocalTime time, LocalDate date) -> {
+			String timeText = time.format(DateTimeFormatter.ofPattern("h:m:s a"));
+			String dateText = date.format(DateTimeFormatter.ofPattern("d MMM yyyy"));
+			System.out.println("Order paced at " + timeText + " on " + dateText);
+		});
 
-	@Given("Customer orders {dishName} dish {int} numbers")
-	public void customer_orders_numbers_of_dish(Dish dish, int quantity) {
-		OrderLine ol = new OrderLine(dish, quantity);
-		System.out.println(ol);
-	}
+		Given("Customer orders {dishName} dish {int} numbers", (Dish dish, Integer quantity) -> {
+			OrderLine orderline = new OrderLine(dish, quantity);
+			System.out.println(orderline);
+		});
 
-	@Given("Customer orders {orderline} dish")
-	public void customer_orders_numbers_of_dish(OrderLine ol) {
-		System.out.println(ol);
+		Given("Customer orders {orderline} dish", (OrderLine orderline) -> {
+			System.out.println(orderline);
+		});
 	}
 }
